0a959de366d5598fa749940471365227c13b4e87
Atabek <atabekarapov14@gmail.com>
2020-10-30 21:25:02 +0600

    Monday

---
+++ Lecture1.py
@@ -0,0 +1,61 @@
+# num1 = 1
+# number_of_students = 23
+# num2 = "HELLO"
+# print(num1)
+# print(number_of_students)
+# number = input("Enter the numbers: ")
+# print(number)
+# print(type(num1))
+# print(type(number))
+
+
+# print(2 * 5**2 / 10 + 8)
+# # a = 20
+# b = 6
+# print(a / b)
+# print(a // b)
+# print(a > b)
+# number = 24
+# number_2 = 5 
+# print(number)
+
+# number = number - number_2
+# number -= number_2
+# number /= number_2
+# print(number)
+#  Homework 
+#  1) 
+a = 5
+b = 10 
+c = 2
+print (c*(a + b))
+
+
+# 2)
+x = 10
+y = 3
+print(x % y)
+
+# 3)
+word = input( "You_are_age: ")
+print (word)
+
+
+
+
+# 4)
+a = 10
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+
+
+
+
+
+
+
+
+
